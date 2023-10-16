15d0f3ba37f011cbeb882929c3bbed1073825a27
Raghu593 <raghurambaddam@gmail.com>
2023-10-16 10:57:32 +0530

    Solved Optimisation Issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState ,useRef} from 'react';
 
 function App() {
+    const email=useRef()
+    const password=useRef()
+    const isFirstRender = useRef(true);
+  
+  const submitHandler=()=>{
+          console.log(email.current.value)
+          console.log(password.current.value)
+  }
+
+  //Changed useEffect to Run Only Once
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+    console.log("Component Mounted");
+  });
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <label>Email</label>
+       <input type='text' ref={email}></input>
+       <label>password</label>
+       <input type='password' ref={password}/>
+       <button onClick={submitHandler}>Submit</button>
       </header>
     </div>
   );
